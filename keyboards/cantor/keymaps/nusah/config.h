ec190dbb8c8b34a475443ed9221588540ecb43b3
Benny Powers <web@bennypowers.com>
2025-08-24 18:09:11 +0300

    nusah: update keymap and config

--- keyboards/cantor/keymaps/nusah/config.h
+++ keyboards/cantor/keymaps/nusah/config.h
@@ -10,18 +10,12 @@
  *  These options are also useful to firmware size reduction.
  */
 
-/* disable debug print */
-//#define NO_DEBUG
-
-/* disable print */
-//#define NO_PRINT
-
 /* disable action features */
 //#define NO_ACTION_LAYER
 //#define NO_ACTION_TAPPING
 //#define NO_ACTION_ONESHOT
 
-/*#define MASTER_RIGHT*/
+#define MASTER_RIGHT
 
 #define SPLIT_WATCHDOG_ENABLE      // https://github.com/diepala/cantor/issues/15#event-13754273368
 #define SPLIT_WATCHDOG_TIMEOUT 3000
